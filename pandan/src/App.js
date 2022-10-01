c72de880182241443d774210ef0c25b61ec5298d
tjsaiz <tjsaizan@yahoo.com>
2022-10-01 11:05:04 -0500

    first draft

--- pandan/src/App.js
+++ pandan/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Select from 'react-select';
 
-function App() {
-  return (
+const degrees = [
+  {label: "Computer Science", value: 1},
+];
+
+const concentrations = [
+  {label: "Software Engineering", value: 1},
+  {label: "Cybersecurity", value: 2}
+];
+
+const years = [
+  {label: "2021", value: 1},
+  {label: "2022", value: 2}
+];
+
+const App = () => (  
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="header">
+            
+        </div>
+        
+        <h1>Welcome to GEAUX DEG</h1>
+        <h2>Please select your degree, concentration, and booklet year.</h2>
+        <div className='selection_box'>
+            <div className='dropdown' name="degree">
+                <Select 
+                  options={degrees} 
+                  placeholder="Select a Degree"
+                  autosize={true}
+                />
+            </div>
+            <div className='dropdown' name="concentration">
+                <Select 
+                  options={concentrations} 
+                  placeholder="Select a Concentration"
+                  autosize={true}
+                />
+            </div>
+            <div className='dropdown' name="year">
+                <Select 
+                  options={years} 
+                  placeholder="Select a Year"
+                  autosize={true}
+                />
+            </div>
+
+            <button>Get Schedule</button>
+        </div>
     </div>
-  );
-}
+);
 
 export default App;
